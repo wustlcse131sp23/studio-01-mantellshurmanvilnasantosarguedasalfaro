46f11e59537e80e23f31c4eaa039522d640cf863
xenairaarguedas <xenairaarguedas@localhost>
2023-01-26 15:49:11 -0600

    hi

---
+++ src/studio1/Average.java
@@ -0,0 +1,19 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Put in a number:");
+		int n1 = in.nextInt();
+		System.out.println("Put in a number:");
+		int n2 = in.nextInt();
+		double average = (n1 + n2)/ 2;
+		System.out.println("Average of " + n1 + "and " + n2 + "is " + average);
+
+	}
+
+}
