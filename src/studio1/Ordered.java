e28de8c73104c9196ea43284d98cde73db915653
alfaroe1 <122934275+alfaroe1@users.noreply.github.com>
2023-01-26 15:29:00 -0600

    Update Ordered.java

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -2,19 +2,28 @@ package studio1;
 
 import java.util.Scanner;
 
-public class Ordered {
+public class Ordered {  
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		
+		boolean isOrdered; 
+		
 		System.out.println("Value for x?");
 		int x = in.nextInt();
 		System.out.println("Value for y?");
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		
+		isOrdered = (x < y && y < z) || (x > y && y > z);
+
+		 
+		System.out.println(isOrdered);
+
 
 	}
 
